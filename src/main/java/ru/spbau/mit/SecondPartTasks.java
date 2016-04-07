41a4d8fba5b5316a9455bc79dd68045f1169b9f5
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2016-04-07 14:47:30 +0300

    Solve second part

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,15 +1,33 @@
 package ru.spbau.mit;
 
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.Supplier;
+import java.util.stream.*;
 
 public final class SecondPartTasks {
+    private static final int NUMBER_OF_SHOTS = 5000;
 
-    private SecondPartTasks() {}
+    private SecondPartTasks() {
+    }
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        Stream<String> allLines = paths.stream().flatMap(f -> {
+            Stream<String> lines;
+            try {
+                lines = Files.lines(Paths.get(f));
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+            return lines;
+        });
+
+        return allLines.filter(l -> l.contains(sequence)).collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -17,18 +35,42 @@ public final class SecondPartTasks {
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
     // какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        final double min = 0;
+        final double max = 1;
+        final double center = 0.5;
+        final double squaredRad = 0.25;
+
+        Supplier<Double> shotSupplier = () -> ThreadLocalRandom.current().nextDouble(min, max);
+
+        Stream<Boolean> shotResults = Stream.generate(() -> {
+            double x = shotSupplier.get();
+            double y = shotSupplier.get();
+            return (x - center) * (x - center) + (y - center) * (y - center) < squaredRad;
+        });
+
+        double numberOfHits = shotResults.limit(NUMBER_OF_SHOTS).filter(x -> x).count();
+
+        return numberOfHits / NUMBER_OF_SHOTS;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        Map<String, Integer> summaryLength = compositions.entrySet().stream().collect(Collectors.toMap(Map
+                .Entry::getKey, p -> p.getValue().stream().mapToInt(String::length).sum()));
+
+        // Use `get` on Option, because operation should fail if map is empty.
+        return summaryLength.entrySet().stream().max(Comparator.comparingInt(Map.Entry::getValue)).map(Map
+                .Entry::getKey).get();
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        Stream<Map.Entry<String, Integer>> totalStream = orders.stream().flatMap(x -> x.entrySet().stream());
+
+        return totalStream.collect(Collectors.groupingBy(Map.Entry::getKey,
+                Collectors.collectingAndThen(Collectors.toList(), x -> x.stream().mapToInt(Map.Entry::getValue)
+                        .sum())));
     }
 }
