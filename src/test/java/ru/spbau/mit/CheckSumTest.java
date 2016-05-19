4d7e20aeaac4fd9a204c62f1c823da52f0e3fc9e
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2016-05-19 18:23:48 +0300

    Add a naive benchmark

--- src/test/java/ru/spbau/mit/CheckSumTest.java
+++ src/test/java/ru/spbau/mit/CheckSumTest.java
@@ -30,10 +30,28 @@ public class CheckSumTest {
             results.add(csc.computeCheckSum(path));
         }
 
-        System.out.println(results.get(0));
-        System.out.println(results.get(1));
-        System.out.println(results.get(2));
-
         assertEquals(results.stream().distinct().count(), 1);
+    }
+
+    @Test
+    public void naiveBenchmark() throws IOException, NoSuchAlgorithmException {
+        List<CheckSumComputer> checkSumComputers = new LinkedList<>();
+        checkSumComputers.add(new SingleThreadCheckSumComputer());
+        checkSumComputers.add(new ForkJoinCheckSumComputer());
+        checkSumComputers.add(new ExecutorServiceCheckSumComputer());
+
+        URL url = this.getClass().getResource("/testDir");
+        File file = new File(url.getFile());
+
+        Path path = file.toPath();
+
+        for (CheckSumComputer csc : checkSumComputers) {
+            long start = System.nanoTime();
+            csc.computeCheckSum(path);
+            long end = System.nanoTime();
+            long nanoTime = end - start;
+            System.out.println(csc.getClass().getSimpleName() + " --- " + (nanoTime / 1000000) + "ms.");
+            System.out.println("----------");
+        }
     }
 }
