f846bc9f7434c02825b7856e55c7b48b5a39731e
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2016-05-19 17:54:24 +0300

    Add a test

---
+++ src/test/java/ru/spbau/mit/CheckSumTest.java
@@ -0,0 +1,37 @@
+package ru.spbau.mit;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class CheckSumTest {
+    @Test
+    public void testCorrectness() throws IOException, NoSuchAlgorithmException {
+        List<CheckSumComputer> checkSumComputers = new LinkedList<>();
+        checkSumComputers.add(new SingleThreadCheckSumComputer());
+        checkSumComputers.add(new ForkJoinCheckSumComputer());
+
+        URL url = this.getClass().getResource("/testDir");
+        File file = new File(url.getFile());
+
+        Path path = file.toPath();
+
+        List<String> results = new LinkedList<>();
+        for (CheckSumComputer csc : checkSumComputers) {
+            results.add(csc.computeCheckSum(path));
+        }
+
+        System.out.println(results.get(0));
+        System.out.println(results.get(1));
+
+        assertEquals(results.stream().distinct().count(), 1);
+    }
+}
